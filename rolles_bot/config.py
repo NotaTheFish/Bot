787af7b6d023e0ed5f41f55cc92b94f20fb2e817
NotaTheFish <edogasha@gmail.com>
2026-06-04 23:01:07 +0200

    Migrations

--- rolles_bot/config.py
+++ rolles_bot/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     ADMIN_ID: int
     DATABASE_URL: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 
 settings = Settings()
